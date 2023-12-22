dad69294508f3897729a124f396b450940fed537
Lissa Kimura Tanaka <lissa.tanaka@hsl.org.br>
2023-12-22 16:56:49 -0300

    Testes Automatizados com Cypress

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,97 @@
+/// <reference types="cypress" />
+
+
+describe('tarefas', ()=> {
+
+    let testData;
+
+    before(()=> {
+        cy.fixture('tasks').then(t => {
+            testData = t
+        })
+    })
+    context ('cadastro', ()=>{
+        it('deve cadastrar uma nova tarefa', ()=>{
+
+            const taskName = 'Testes automatizados'
+    
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+    
+            cy.contains('main div p',taskName)
+                .should('be.visible')
+    
+        })
+    
+        it('não deve permitir cadastro de uma tarefa duplicada', ()=>{
+            
+            const task = testData.dup
+    
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
+    
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!' )
+    
+        })
+    
+        it('campo obrigatório', ()=>{
+            cy.createTask()
+            cy.isRequired('This is a required field')
+    
+        })
+    })
+
+    context('atualização', ()=>{
+        it('deve concluir uma tarefa', ()=>{
+            const task = {
+                name:'Testes funcionais',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
+            
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
+
+
+        })
+
+    })
+
+
+    context('exclusão', ()=>{
+        it('deve remover uma tarefa', ()=>{
+            const task = {
+                name:'Criação de massa',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemDelete]')
+                .click()
+            
+            cy.contains('p', task.name)
+                .should('not.exist')
+
+
+        })
+
+    })
+})
